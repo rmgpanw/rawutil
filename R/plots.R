25744a7698e30bd75438592a7bbf9b4b02153846
rmgpanw <rmgpanw@ucl.ac.uk>
2021-03-17 12:15:12 +0000

    amend plot_heatmap(), can now filter and order selected values on axes, and has option to annotate tiles

--- R/plots.R
+++ R/plots.R
@@ -14,7 +14,9 @@
 #' @param x character
 #' @param y character
 #' @param fill character
-#' @param x_order chracter vector
+#' @param label character. Annotate tiles
+#' @param label_size numeric. Text size for tile annotations
+#' @param x_order chracter vector.
 #' @param y_order character vetor
 #' @param xlab character
 #' @param ylab character
@@ -31,6 +33,8 @@ plot_heatmap <- function(df,
                          x,
                          y,
                          fill,
+                         label = NULL,
+                         label_size = NULL,
                          x_order = NULL,
                          y_order = NULL,
                          xlab = "x title",
@@ -42,19 +46,46 @@ plot_heatmap <- function(df,
                          ylab_text_size = NULL,
                          geom_tile_color = "white") {
 
-  # order x and y axes, if specified
+  # order and filter x/y axes, if specified
   if (!is.null(x_order)) {
+    # warning if any items in x_order or NOT in df[[x]]
+    if (any(!x_order %in% df[[x]])) {
+      warning("x_order contains values that are not present in df[[x]]")
+    }
+
+    # filter
+    df <- df %>%
+      dplyr::filter(.data[[x]] %in% x_order)
+
+    # order
     df[[x]] <- factor(df[[x]], levels = x_order)
   }
 
   if (!is.null(y_order)) {
+    # warning if any items in x_order or NOT in df[[x]]
+    if (any(!y_order %in% df[[y]])) {
+      warning("y_order contains values that are not present in df[[y]]")
+    }
+
+    # filter
+    df <- df %>%
+      dplyr::filter(.data[[y]] %in% y_order)
+
+    # order
     df[[y]] <- factor(df[[y]], levels = y_order)
   }
 
   # plot
-  df %>%
+  heatmap <- df %>%
     ggplot2::ggplot(ggplot2::aes(x = .data[[x]], y = .data[[y]], fill = .data[[fill]])) +
     ggplot2::geom_tile(color = geom_tile_color) +
+
+    # axis and legend labels
+    ggplot2::xlab(xlab) +
+    ggplot2::ylab(ylab) +
+    ggplot2::labs(fill = legend_lab) +
+
+    # formatting
     # scale_fill_distiller(palette = palette) +
     ggplot2::theme(
       axis.text.x = ggplot2::element_text(angle = xlab_text_angle, size = xlab_text_size),
@@ -62,9 +93,6 @@ plot_heatmap <- function(df,
       axis.ticks.x = ggplot2::element_blank(),
       axis.ticks.y = ggplot2::element_blank(),
     ) +
-    ggplot2::xlab(xlab) +
-    ggplot2::ylab(ylab) +
-    ggplot2::labs(fill = legend_lab) +
 
     # make tiles square
     ggplot2::coord_equal() +
@@ -72,6 +100,15 @@ plot_heatmap <- function(df,
     # remove grey border around heatmap plot
     ggplot2::scale_x_discrete(expand = c(0, 0)) +
     ggplot2::scale_y_discrete(expand = c(0, 0))
+
+  # annotate tiles - TODO warning if only one of these is specified?
+  if (!is.null(label) & !is.null(label_size)) {
+    heatmap <- heatmap + geom_text(aes(label = .data[[label]]),
+                                   size = label_size)
+  }
+
+  # return heatmap
+  return(heatmap)
 }
 
 # PRIVATE FUNCTIONS -------------------------------------------------------
