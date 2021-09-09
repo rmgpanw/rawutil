b572c9011e14763b90433b195a1f1a1db6d4df79
rmgpanw <rmgpanw@ucl.ac.uk>
2021-09-09 16:29:28 +0100

    update prev commit

--- R/utils.R
+++ R/utils.R
@@ -240,8 +240,8 @@ print_df_as_call_to_tibble <- function(df) {
                ),
                "),\n") |>
           # for NA values in character columns, convert 'NA' to NA
-          stringr::str_replace_all(pattern = "'NA',?",
-                                   replacement = " NA, "))
+          stringr::str_replace_all(pattern = "'NA'",
+                                   replacement = "NA"))
   }
 }
 
